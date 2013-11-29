0667b2a7f6053217dd23b663095c952075d294bf
Gene Sy <gnsytw@gmail.com>
2013-11-29 18:08:58 +0800

    edited build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-rmdir dist
+rm -rf dist
 r.js -o app/build/app.build.js
 cd dist
 rm -rf build build.txt js/views js/vendor bower_components js/foundation js/backbone.localStorage-min.js js/foundation.min.js js/models js/collections js/views js/router.js fbower_components sass config.rb 
